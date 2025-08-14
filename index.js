1c213518cca70d50279083fd4f9a28cede59b6f8
jaymar422 <225466549+jaymar422@users.noreply.github.com>
2025-08-14 17:53:07 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// deploy repo is closed no longer available for any type of deployment 👀
+
+// Deploy From New Repository ✅ 
+
+// JAYMAR-MD version 2.0 ❤️
+
+// Thanks For Chosing JAYMAR-MD 🔥
+
+// Made with ♥️ By JayTech
+
+// Powered By JayTech JAYMAR-MD 2025 🚀
